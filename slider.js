c657fec5399ad42375d11ec1901ec2c7fb2218c0
anchal949 <anchalsrivastava5203210@gmail.com>
2019-10-05 21:53:55 +0530

    typed.js added

--- slider.js
+++ slider.js
@@ -37,3 +37,18 @@ function showSlides() {
   slides[slideIndex-1].style.display = "block";
   setTimeout(showSlides, 2000); 
 }
+
+var mybutton = document.getElementById("myBtn");
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
